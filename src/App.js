e19de35658739817e60fc312a6cc827adaad527a
Meet Panchal <connect.meetpanchal@gmail.com>
2019-12-02 01:32:47 +0530

    Edit button left

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserTables from './tables/UserTable'
+import AddUserForm from './forms/AddUserForm'
 
-function App() {
+const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Meet', username: 'meet.6338' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+  const [editing, setEditing] = useState(false)
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  const initialFormState = { id: null, name: '', username: '' }
+  const addUser = user => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+  const editRow = user => {
+    setEditing(true)
+    setCurrentUser({ id: user.id, name: user.name, username: user.username })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+          <AddUserForm addUser={addUser} />
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTables users={users} editRow={editRow} deleteUser={deleteUser}/>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
